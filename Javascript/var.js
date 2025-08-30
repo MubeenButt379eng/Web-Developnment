8cde33ac9728d81afa397bf8995176d3b555ab4d
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-08-30 15:19:56 +0500

    Added Variable

--- Javascript/var.js
+++ Javascript/var.js
@@ -1,2 +1,23 @@
-console.log("This is a variable example in JavaScript.");
-var name = "Mubeen Butt";
+ //todo Variable in js 
+
+ const accountId = 14448;
+ let accountEmail = "Mubeen@gmail.com";
+ var accountPassword = "12345";
+    accountCity = "Karachi";
+    let accountState;
+
+    // * accountId = 24;  nor allowed in js
+
+
+    accountEmail = "hh@gmail.com";
+    accountPassword = "67890";
+    accountCity = "Lahore";
+
+    console.log(accountId);
+    
+    console.table([accountId,accountEmail, accountPassword, accountCity]);
+
+    //* prefer not to use var beacuse of its function scope
+
+
+    
