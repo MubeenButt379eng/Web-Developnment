9c0e561180fa7a86e70bb5ac88c0ce1b4986d697
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-08-30 15:44:58 +0500

    Added variable

---
+++ Javascript/1var.js
@@ -0,0 +1,23 @@
+ //todo Variable in js 
+
+ const accountId = 14448;
+ let accountEmail = "Mubeen@gmail.com";
+ var accountPassword = "12345";
+    accountCity = "Karachi";
+    let accountState;
+
+    // * accountId = 24;  nor allowed in js
+
+
+    accountEmail = "hh@gmail.com";
+    accountPassword = "67890";
+    accountCity = "Lahore";
+
+    console.log(accountId);
+    
+    console.table([accountId,accountEmail, accountPassword, accountCity]);
+
+    //* prefer not to use var beacuse of its function scope
+
+
+    
