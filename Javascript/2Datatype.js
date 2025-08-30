0f6e437efeac9f7f28497cca5710ebe1ef8fd3f7
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-08-30 16:24:08 +0500

    Added

---
+++ Javascript/2Datatype.js
@@ -0,0 +1,18 @@
+//todo Datatype in js 
+
+"use strict"; // treat all js code as modern js version
+
+
+let name = "MubeenButt"; //? string
+let age = 22; // number
+let isApproved = false; //boolean
+
+
+//* this is primitive datatype
+//todo number , bigint , string , boolean , null , undefined , symbol
+
+//* this is non-primitive datatype
+ // object , array , function 
+
+console.log(typeof null);// object
+console.log(typeof undefined); // undefined 
