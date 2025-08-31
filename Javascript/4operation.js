e83e219b605fa5f46f2a4faa52bb543baf0844d8
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-08-31 14:08:07 +0500

    Operation in js Added

---
+++ Javascript/4operation.js
@@ -0,0 +1,38 @@
+//todo operation in js
+
+let value = 3;
+let negValue = -value;
+console.log(negValue); // -3
+
+
+/*console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3); // mean 2 power 3
+console.log(2/3);
+console.log(2%3);// mean 2 mod3
+
+*/
+
+let str1 = "hello ";
+
+let str2 = " world";
+console.log(str1 + str2); // hello world
+
+/*
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log(1 + 2 + "3"); // 33
+console.log("1" + 2 + 3); // 123
+*/
+
+console.log(+true);
+
+
+let myCounter = 10;
+// ++myCounter;
+console.log(myCounter++);
+console.log(myCounter);
+
+
+
