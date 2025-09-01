45e601acbc1bc26d07484cc257cf1c2602bb40b8
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-09-01 21:59:16 +0500

    Datatype Summary

---
+++ Javascript/6.datatypeSummary.js
@@ -0,0 +1,33 @@
+//todo primitive data type (call by value) 
+
+// *1. number, string, boolean, null, undefined, symbol, bigint
+
+//*JavaScript dynamically typed language hai.
+
+const score = 100;
+
+const isLoggedIn = false;
+
+const outsideTemp = null;
+
+let userEmail; //undefined
+
+const id = Symbol("123");
+
+const bigNumber = 3456789012n;
+
+//todo reference data type mean k non primitive (call by reference)
+
+//* Array , object and function
+
+
+const name = ["aman", "raj", "rohit"];
+
+const person = {
+    name: "aman",
+    age: 22,
+};
+
+const myFunction = function(){
+    console.log("hello world");
+}
