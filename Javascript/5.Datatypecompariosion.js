e345d952dfa63904235a5691094fe4953fd8f9a5
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-09-01 21:34:37 +0500

    Comparision of datatape

---
+++ Javascript/5.Datatypecompariosion.js
@@ -0,0 +1,19 @@
+//todo comparison of datatypes
+
+
+console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+//todo === check the value and datatype strict equality operator
+
+console.log("2" ===2); // always write clean code don't use difficult code 
