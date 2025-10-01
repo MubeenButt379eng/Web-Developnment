6acfde66879e41e54900b0d0ad85648a5cf434ff
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-10-01 13:17:53 +0500

    Scope added

--- Javascript/16_scope.js
+++ Javascript/16_scope.js
@@ -13,3 +13,43 @@ if (true) {
 console.log(a);
 
 //todo donot use var because it is function scoped and can lead to unexpected behavior.
+
+//*nested function scope
+
+function one(){
+    const userName= "Mubeen"
+
+    function two(){
+const website= "youtube"
+console.log(userName);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if (true){
+    const userName= "Mubeen"
+    if (userName === "Mubeen"){{
+        const website= " youtube"
+        // console.log(userName + website);
+    }
+    // console.log(website);
+}
+// console.log(userName);
+}
+
+
+//todo ++++++++++++++++++
+
+function addOne(num){
+return num + 1
+}
+addOne(5)
+
+
+//* functio expression as a variable
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
