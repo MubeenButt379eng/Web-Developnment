f31d05a8328e322d33bb20359c46f13e51a093bf
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-10-04 20:56:32 +0500

    Arrow function

---
+++ Javascript/17_this&that.js
@@ -0,0 +1,31 @@
+const user={
+userName: "mubeen",
+price:999,
+welcomeMessage: function(){
+    console.log(`${this.userName}, welcome to website`);
+    // console.log(this);
+}
+}
+// user.welcomeMessage();
+// console.log(this); //todo answer is empty
+
+// function one(){
+//     let userName = "mubeen";
+//     console.log(this.userName); //todo cannot accss this in function but object access it 
+// }
+// one();
+
+
+// const one = () => {
+//     let userName = "mubeen";
+// console.log(this.userName);
+// }
+
+// const addTwo = (a , b)=>{
+// return a+b;
+// }
+
+const addTwo = (a , b)=>   a+b;
+    
+    
+console.log(addTwo(3,4));
