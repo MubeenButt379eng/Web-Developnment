417c3d328746e36ab6f68513b643750856a150cf
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-10-06 13:02:04 +0500

    while and do while loop

---
+++ Javascript/21_while$doWhile.js
@@ -0,0 +1,26 @@
+// while (condition) {
+    
+// }
+
+let i = 0;
+while(i<=10){
+    // console.log(`value of i is ${i}`);
+    // i++;
+    i = i +2
+}
+
+let myArray = ["hhh","bbbbb","sssssss"]
+let arr =0;
+while (arr< myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr+1;
+}
+
+
+let score = 1;
+
+do {
+    // console.log(`score si ${score}`);
+    score++
+} while (score <= 10);
+
