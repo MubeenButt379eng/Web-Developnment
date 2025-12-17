b28910f6d495777c1819c6d9359eb611ba82142d
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-12-17 21:39:21 +0500

    call and this in js

--- Javascript/classes_and_oop/call.js
+++ Javascript/classes_and_oop/call.js
@@ -13,3 +13,12 @@ function createUser(userName,email,password){
 const chai = new createUser("chai","cahi@email.com","123")
 console.log(chai);
  
+
+// **.call() Method: "Zabardasti Connection"
+// *Kabhi kabhi hum chahte hain ke ek function kisi aise object ke liye kaam kare jiska wo hissa nahi hai. .call() humein ijazat deta hai ke hum manually bata sakein ke this ki value kya honi chahiye.
+
+// *Istemal kyun karte hain?
+
+// *Function Borrowing: Ek object ka function doosre object ke liye use karna.
+
+// *Context Pass karna: this ko fix karna taake wo sahi object ko point kare.
